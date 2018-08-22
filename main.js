e8492ef96da2aa4d8d9994f930f19bdf299b98e3
Jonathan Gallo <jonnygallo@gmail.com>
2018-08-22 09:43:49 -0700

    added structure to html and js files

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
- 
+$(document).ready(initializeGame);
+
+var player1;
+var player2;
+
+
+
+
+
+
+
+function initializeGame () {
+
+
+
+}
+
+
+
+function createGameboard () {
+
+
+
+}
+
+
+
+function keepScore () {
+
+
+
+}
+
+
+
+function playersTurn () {
+
+
+
+}
+
+
+
+function CheckForWin() {
+
+
+
+}
+
+
+function resetGame () {
+
+
+
+}
+
